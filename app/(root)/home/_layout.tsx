033df8d6f038b144635cfa0bbcf76a66740252f5
Jeroen <jgroenendijk@enrise.com>
2023-02-06 15:08:37 +0100

    add problem case

--- app/(root)/home/_layout.tsx
+++ app/(root)/home/_layout.tsx
@@ -1,3 +1,10 @@
 import {Drawer} from "expo-router/drawer";
 
-export default Drawer;
+export default function () {
+    return (
+        <Drawer>
+            <Drawer.Screen name="drawer-test-page" options={{title: 'test page'}} />
+            <Drawer.Screen name="/help" options={{title: 'help'}} />
+        </Drawer>
+    );
+};
