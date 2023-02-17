9d5f0a8aeff339300d98398acbb51bfc53acd538
Jeroen <jgroenendijk@enrise.com>
2023-02-17 12:34:19 +0100

    solution

--- app/(root)/_layout.js
+++ app/(root)/_layout.js
@@ -6,6 +6,7 @@ export default function Layout() {
         <Tabs.Screen name="index" options={{title: 'Home'}} />
         <Tabs.Screen name="alpha" options={{title: 'Alpha'}} />
         <Tabs.Screen name="beta" options={{title: 'Beta'}} />
+        <Tabs.Screen name="charlie" options={{ href: null }} />
     </Tabs>
   );
 }
--- app/_layout.js
+++ app/_layout.js
@@ -1,12 +1,22 @@
 import {Drawer} from "expo-router/drawer";
+import {DrawerContentScrollView, DrawerItem} from "@react-navigation/drawer";
+import {Linking} from "react-native";
+import {Link} from "expo-router";
+
+function CustomDrawerContent(props) {
+    return (
+        <DrawerContentScrollView {...props}>
+            <DrawerItem label="Website" onPress={() => Linking.openURL('https://www.expo.dev/')} />
+            <Link href={'/alpha'} onPress={() => props.navigation.closeDrawer()}>Alpha</Link>
+            <Link href={'/beta'} onPress={() => props.navigation.closeDrawer()}>Beta</Link>
+            <Link href={'/charlie'} onPress={() => props.navigation.closeDrawer()}>Charlie</Link>
+        </DrawerContentScrollView>
+    );
+}
 
 export default function Layout() {
     return (
-        <Drawer>
-            <Drawer.Screen name="/" options={{title: 'Home'}} />
-            <Drawer.Screen name="alpha" options={{title: 'Alpha'}} />
-            <Drawer.Screen name="beta" options={{title: 'Beta'}} />
-            <Drawer.Screen name="charlie" options={{title: 'Charlie'}} />
+        <Drawer drawerContent={(props) => <CustomDrawerContent {...props} />}>
             {/* All other screens should be hidden */}
         </Drawer>
     );
